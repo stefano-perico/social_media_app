dc70ab250da26acb32d05677518c926b3475f44c
stefanoperico <stefano.perico@inter-invest.fr>
2023-09-19 09:04:28 +0200

    add components to login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_app/components/button.dart';
+import 'package:social_media_app/components/text_field.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,32 +10,87 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final emailTextController = TextEditingController();
+  final passwordTextController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 50,),
-              // logo
-              const Icon(
-                Icons.lock,
-                size: 100,
-              ),
-              const SizedBox(height: 50,),
-              // welcome back message
-              const Text(
-                "Welcome Back!",
-              )
-              // email textfield
-        
-              // password textfield
-        
-              // sign in button
-        
-              // go to register page button
-            ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // logo
+                const Icon(
+                  Icons.lock,
+                  size: 100,
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                // welcome back message
+                Text(
+                  "Welcome Back!",
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                // email textfield
+                MyTextField(
+                  controller: emailTextController,
+                  hintText: 'Email',
+                  obscureText: false
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                // password textfield
+                MyTextField(
+                  controller: passwordTextController,
+                  hintText: 'Password',
+                  obscureText: true
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                // sign in button
+                MyButton(onTap: () => {}, text: 'Sign In'),
+                const SizedBox(
+                  height: 25,
+                ),
+                // go to register page button
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account?",
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                      )
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    GestureDetector(
+                      onTap: () => {},
+                      child: const Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       )
