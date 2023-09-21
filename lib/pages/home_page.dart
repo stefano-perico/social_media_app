d3c43c6bcfd00f1b68a35ed13ba5416c105d9ca6
stefanoperico <stefano.perico@inter-invest.fr>
2023-09-21 08:47:24 +0200

    add sign out

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,11 +9,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  // sign out function
+  void signOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("The Wall"),
+        actions: [
+          // sign out button
+          IconButton(onPressed: signOut, icon: const Icon(Icons.logout))
+        ],
       ),
       body: const Center(
         child: Text("Home Page"),
